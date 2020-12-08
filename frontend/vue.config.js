547dfdd2472d0923ad3189865f84b3ab63a2fb15
Radu <udarnicus@gmail.com>
2020-12-08 11:59:15 +0100

    Changed Structure

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,3 @@
-// Vue CLI configuration, see https://cli.vuejs.org/config
 module.exports = {
-  // configure the port the development server will run on
-  devServer: {
-    port: 8000
-  },
-  // display eslint errors as warnings --> build does not fail on linting issues
-  lintOnSave: true
-};
+  publicPath: '/'
+}
