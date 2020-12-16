b4f99e7b081eee483140c552de1dc4db640d4080
Radu <udarnicus@gmail.com>
2020-12-16 23:15:34 +0100

    Working on the Charts for Clients

--- frontend/src/views/components/dashboard/BarChart.vue
+++ frontend/src/views/components/dashboard/BarChart.vue
@@ -1,25 +1,76 @@
 <script>
 import { Bar } from "vue-chartjs";
+import axios from "axios";
 
 export default {
   extends: Bar,
+  props: ['clientID'],
+
+  data: () => {
+    return {
+      registrations:[],
+      dateLabels:["","","","",""],
+      workedHours:[0,0,0,0,0]
+    };
+  },
+
+  methods: {
+    async fetchRegistrations(){
+      await axios.get(`http://localhost:8080/timeregistrations`)
+          .then(response => {
+            // JSON responses are automatically parsed.
+            for (var i = 0; i < response.data.length; i++) {
+              if(response.data[i].id === this.props.clientID){
+                if(this.dateLabels.includes(response.data[i].start.slice(5,10))){
+                  this.registrations.concat(response.data[i])
+                }
+
+              }
+            }
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+
+    },
+    getDateLabels(){
+      var today = new Date();
+      for(var i = 0;i<5;i++){
+        var dd = String(today.getDate()-4+i).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
+        this.dateLabels[i] =  mm + "-" + dd
+      }
+    },
+    getWorkedHours(){
+      for(var i = 0; i < this.registrations.length;i++){
+        for(var j= 0; j<this.dateLabels.length;j++){
+          if(this.registrations[i].start.slice(5,10).localeCompare(this.dateLabels[j])){
+            var startDate = Date.parse(this.registrations[j].start);
+            var endDate = Date.parse(this.registrations[j].end);
+            var hours = Math.abs(endDate - startDate) / 36e5;
+            this.workedHours[j] = this.workedHours[j] + hours
+          }
+
+        }
+      }
+
+    },
+  },
+
+
   mounted() {
+    this.getDateLabels();
+    this.fetchRegistrations();
+    this.getWorkedHours();
+
     this.renderChart(
       {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
-        ],
+        labels: this.dateLabels,
         datasets: [
           {
-            label: "Income $",
+            label: "Worked Time $",
             backgroundColor: "#2962ff",
-            data: [2000, 40000, 20000, 39000, 10000, 40000, 69000],
+            data: this.workedHours,
           }
         ]
       },
--- frontend/src/views/components/sopra/customerList.vue
+++ frontend/src/views/components/sopra/customerList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="table-responsive">
     <vs-row vs-justify="center">
-      <vs-col type="flex" vs-justify="center" vs-align="center" :vs-lg="clientSelected ? 6 : 12" vs-sm="6" vs-xs="12" code-toggler>
+      <vs-col type="flex" vs-justify="center" vs-align="center" :vs-lg="clientSelected ? 4 : 12" vs-sm="6" vs-xs="12" code-toggler>
         <vs-card class="cardx">
         <table class="table v-middle border">
           <thead>
@@ -37,7 +37,7 @@
         </table>
         </vs-card>
       </vs-col>
-      <vs-col v-if="clientSelected" type="flex" vs-justify="center" vs-align="center" vs-sm="6" vs-lg="6" vs-xs="12">
+      <vs-col v-if="clientSelected" type="flex" vs-justify="center" vs-align="center" vs-sm="6" vs-lg="4" vs-xs="12">
         <vs-card v-show="clientSelected" class="cardx">
           <div slot="header">
             <h4 >Details vom {{currentClient.name}} </h4>
@@ -57,6 +57,9 @@
           </div>
         </vs-card>
       </vs-col>
+      <vs-col v-if="clientSelected" type="flex" vs-justify="center" vs-align="center" vs-sm="6" vs-lg="4" vs-xs="12"><vs-card class="cardx">
+        <ClientChart :clientID="this.currentClient.clientID"/>
+        </vs-card></vs-col>
       <vs-button @click="activePrompt = true" color="primary" type="filled">Add Customer</vs-button>
       <vs-prompt
         title="Add Client"
@@ -114,9 +117,13 @@
 
 <script>
 import axios from 'axios';
+import ClientChart from "@/views/components/dashboard/ClientChart";
 
 export default {
   name: "clientList",
+  components: {
+    ClientChart,
+  },
   data: () => {
     return {
       clients: [],
