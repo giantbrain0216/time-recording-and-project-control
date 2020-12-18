5542c9f159ff11990ab7176aead67e8ee773e9de
Radu <udarnicus@gmail.com>
2020-12-18 18:12:00 +0100

    Added Time Registration List

--- frontend/src/views/components/sopra/timeRegistration.vue
+++ frontend/src/views/components/sopra/timeRegistration.vue
@@ -50,6 +50,49 @@
 
       </vs-card>
     </vs-col>
+    <vs-col  v-if="timeregistrations.length!==0" type="flex" vs-justify="center" vs-align="center" :vs-lg="8" vs-sm="6" vs-xs="12" code-toggler>
+      <vs-card class="cardx">
+        <div slot="header">
+          <h4>
+            {{"Time Registrations for Employee with ID: " + currentEmployeeID}}
+          </h4>
+        </div>
+        <div>
+        <table class="table v-middle border">
+          <thead>
+          <tr class="">
+            <th class="border-top-0">Project</th>
+            <th class="border-top-0">Registration ID</th>
+            <th class="border-top-0">From</th>
+            <th class="border-top-0">To</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="registration in timeregistrations" :key="registration.id">
+            <td>
+              <div class="d-flex align-items-center">
+                <div class="mr-2">{{projectNames[registration.projectID]}}</div>
+              </div>
+            </td>
+            <td>{{registration.id}}</td>
+            <td><div class="d-flex align-items-center">{{registration.start}}</div></td>
+            <td><div class="d-flex align-items-center">{{registration.end}}</div></td>
+            <td>
+              <div>
+                <vs-button class="m-1" color="danger" type="filled">
+                  Delete
+                </vs-button>
+                <vs-button class="m-1" color="primary" type="filled">
+                  Edit
+                </vs-button>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        </div>
+      </vs-card>
+    </vs-col>
     </vs-row>
   </div>
 </template>
@@ -62,6 +105,7 @@ export default {
   data: () => {
     return {
       projects:[],
+      projectNames:{},
       currentProject: "Project",
       currentProjectID: 0,
       employees:[],
@@ -69,12 +113,13 @@ export default {
       currentEmployeeID:0,
       dateToday: "",
       starttime: "",
-      endtime: ""
+      endtime: "",
+      timeregistrations:[]
     };
   },
 
-  created() {
-    axios.get(`http://localhost:8080/projects`)
+   async created() {
+    await axios.get(`http://localhost:8080/projects`)
         .then(response => {
           // JSON responses are automatically parsed.
           this.projects = response.data
@@ -82,7 +127,7 @@ export default {
         .catch(e => {
           this.errors.push(e)
         })
-    axios.get(`http://localhost:8080/employees`)
+     await axios.get(`http://localhost:8080/employees`)
         .then(response => {
           // JSON responses are automatically parsed.
           this.employees = response.data
@@ -90,6 +135,9 @@ export default {
         .catch(e => {
           this.errors.push(e)
         })
+
+
+
 
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
@@ -101,6 +149,29 @@ export default {
   },
 
   methods: {
+    fetchTimeRegistrations: async function(id){
+      this.timeregistrations = []
+      await axios.get(`http://localhost:8080/timeregistrations`)
+          .then(response => {
+            // JSON responses are automatically parsed.
+            for (let i = 0; i < response.data.length; i++) {
+              console.log("Comparing " + response.data[i].employeeID+ "with " + id)
+              if(response.data[i].employeeID === id){
+                  this.timeregistrations.push(response.data[i])
+              }
+            }
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+
+      for(var i=0;i<this.projects.length;i++){
+        //this.projectNames.push({key:this.projects[i].projectID ,value: this.projects[i].projectName})
+        this.projectNames[this.projects[i].projectNumber] = this.projects[i].projectName
+      }
+    },
+
+
     updateProject: function(id){
         this.currentProjectID = id;
       for(let i = 0; i<this.projects.length;i++){
@@ -110,7 +181,8 @@ export default {
       }
     },
 
-    updateEmployee: function(id){
+    updateEmployee: async function(id){
+      await this.fetchTimeRegistrations(id)
       this.currentEmployeeID = id;
       for(let i = 0; i<this.employees.length;i++){
         if(this.employees[i].employeeID == id){
@@ -140,6 +212,7 @@ export default {
       var enddate = new Date("1970-01-01 " + endtime);
       return (startdate.getTime() < enddate.getTime())
     },
+
 
   }
 
