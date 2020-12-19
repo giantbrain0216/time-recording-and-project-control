e7f2b40f0e04c24839432c3367375c62daad35ad
Mohamed Ben Salha <st167263@stud.uni-stuttgart.de>
2020-12-20 00:47:54 +0100

    ProjectDatabaseTest fixed

--- api/src/test/java/database/methods/test/ProjectDatabaseTest.java
+++ api/src/test/java/database/methods/test/ProjectDatabaseTest.java
@@ -32,9 +32,8 @@ public class ProjectDatabaseTest {
 
     /**
      * This method tests whether an object can be successfully created in the
-     * database by checking after the insertion whether the size of the list has
-     * increased by 1 and whether the data of the added project is stored in the
-     * correct columns.
+     * database by checking after the insertion whether the  id of the added
+     * project is in the list of all existing projects
      *
      * @throws SQLException if the project has not been added
      */
@@ -43,28 +42,28 @@ public class ProjectDatabaseTest {
         Project project = new Project("Project Test",21, new Date(11 - 11 - 2020),
                 new Date(2020 - 11 - 11), 100,  "HTML-CSS");
         projectDatabase.addToDatabase(project);
-
-        Assert.assertTrue(projectDatabase.getAllProjects().contains(project));
-        System.out.println(project.getProjectNumber());
+        Assert.assertTrue(projectDatabase.getAllProjects().stream().anyMatch(project1 -> project1.getProjectNumber().equals(project.getProjectNumber())));
         projectDatabase.deleteFromDatabase(project.getProjectNumber());
-        Assert.assertFalse( projectDatabase.getAllProjects().contains(project));
+
 
     }
 
     /**
      * This method tests whether an object can be successfully deleted from the
-     * database by checking after the deletion whether the size of the list has
-     * decreased by 1.
+     * database by checking after the deletion whether its id does not belong
+     * to he ids of the existing projects
      *
      * @throws SQLException if the project has not been deleted
      */
     @Test
     public void deleteProjectTest() throws SQLException {
-        assertEquals(1, projectDatabase.getAllProjects().size());
-        projectDatabase.deleteFromDatabase(1);
-        assertEquals(0, projectDatabase.getAllProjects().size());
-        projectDatabase.addToDatabase(new Project("Project Test",21, new Date(11 - 11 - 2020),
-                new Date(2020 - 11 - 11), 268,  "HTML"));
+        Project project = new Project("Project Test",21, new Date(11 - 11 - 2020),
+                new Date(2020 - 11 - 11), 268,  "HTML");
+        projectDatabase.addToDatabase(project);
+        Assert.assertTrue(projectDatabase.getAllProjects().stream().anyMatch(project1 -> project1.getProjectNumber().equals(project.getProjectNumber())));
+        projectDatabase.deleteFromDatabase(project.getProjectNumber());
+        Assert.assertFalse(projectDatabase.getAllProjects().stream().anyMatch(project1 -> project1.getProjectNumber().equals(project.getProjectNumber())));
+
     }
     /**
      * This method tests whether an object can be successfully modified in the
@@ -72,18 +71,18 @@ public class ProjectDatabaseTest {
      * been saved in the database and the old one has been overwritten.
      */
     @Test
-    public void modifyProjectDataTest(){
-        Project project = projectDatabase.getProject(1);
+    public void modifyProjectDataTest() throws SQLException {
+        Project project = new Project("Project Test",21, new Date(11 - 11 - 2020),
+                new Date(2020 - 11 - 11), 268,  "HTML");
+        projectDatabase.addToDatabase(project);
         Assert.assertEquals("HTML",project.getCompetences());
-        Assert.assertEquals(14  , project.getPerformedEffort());
+        Assert.assertEquals(0  , project.getPerformedEffort());
         project.addCompetence("ANGULAR");
         project.setPerformedEffort(90);
         projectDatabase.modifyProjectData(project);
         Assert.assertEquals("HTML-ANGULAR",project.getCompetences());
         Assert.assertEquals(90  , project.getPerformedEffort());
-        project.setPerformedEffort(14);
-        project.removeCompetence("ANGULAR");
-        projectDatabase.modifyProjectData(project);
+        projectDatabase.deleteFromDatabase(project.getProjectNumber());
 
     }
 }
