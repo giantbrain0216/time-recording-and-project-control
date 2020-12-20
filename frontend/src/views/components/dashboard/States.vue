8c0220ff650672ac1fcbeb34eb9312c883a3f945
ClirimSalihi <st170171@stud.uni-stuttgart.de>
2020-12-20 13:23:25 +0100

    changes from befor

--- frontend/src/views/components/dashboard/States.vue
+++ frontend/src/views/components/dashboard/States.vue
@@ -23,9 +23,9 @@
         </vs-col>
         <vs-col-- vs-lg="3" vs-xs="12">
             <vs-card>
-                <h4 class="mb-1">30%</h4>
-                <span>Company Growth</span>
-                <vs-progress :percent="30" color="warning">primary</vs-progress>
+                <h4 class="mb-1">{{ Math.round(calculateCancelled())}}%</h4>
+                <span>Projects are cancelled</span>
+                <vs-progress :percent="calculateCancelled()" color="warning">primary</vs-progress>
             </vs-card>
         </vs-col-->
     </vs-row>    
@@ -129,7 +129,22 @@ export default {
       }
       return (nrProjectsNeedEmployees/nrProjects)
 
-    }
+    },
+
+    calculateCancelled: function () {
+      let today = new Date()
+      let countRunning = 0
+      let countCancelled = 0
+      for (let i = 0; i < this.projects.length; i++) {
+        let deadline = new Date(this.projects[i].plannedEnd)
+        if (today.getTime() > deadline.getTime()) {
+          countRunning += 1
+        } else {
+          countCancelled += 1
+        }
+      }
+      return countRunning/countCancelled*100
+    },
   }
 }
 </script>
