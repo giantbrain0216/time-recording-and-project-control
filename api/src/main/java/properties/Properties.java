c4fbfe166ecdd214549a4f21180b1d5ceaf16243
Radu <udarnicus@gmail.com>
2020-12-27 19:27:53 +0100

    Added Properties File for Database

--- api/src/main/java/properties/Properties.java
+++ api/src/main/java/properties/Properties.java
@@ -6,11 +6,11 @@ public final class Properties {
         // restrict instantiation
     }
 
-    /*public static final String LINK = "bilbao.informatik.uni-stuttgart.de/team12";
+    public static final String LINK = "bilbao.informatik.uni-stuttgart.de/team12";
     public static final String USERNAME = "team12";
-    public static final String PASSWORD = "e345c973d6f4d770";*/
+    public static final String PASSWORD = "e345c973d6f4d770";
 
-    public static final String LINK = "192.168.0.200:3306/team12";
+   /* public static final String LINK = "192.168.0.200:3306/team12";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "incorrect";
+    public static final String PASSWORD = "incorrect";*/
 }
