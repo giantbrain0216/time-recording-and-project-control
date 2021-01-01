eb3dd220dc1c1db88b8bad134033d7bcbd0dbec9
Mohamed Ben Salha <st167263@stud.uni-stuttgart.de>
2021-01-01 21:09:03 +0100

    Competence as a class and a table in the database added

---
+++ api/src/main/java/entities/Competence.java
@@ -0,0 +1,34 @@
+package entities;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import javax.validation.constraints.NotNull;
+
+@DatabaseTable(tableName = "Competences")
+public class Competence {
+
+    @DatabaseField(columnName = "ID", id = true)
+    @NotNull
+    private int id;
+
+    @DatabaseField(columnName = "Name", id = true)
+    @NotNull
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
