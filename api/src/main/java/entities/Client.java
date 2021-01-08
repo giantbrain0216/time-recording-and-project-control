a06632c8f8236031f1629a5c4c568970d1489640
Mohamed Ben Salha <st167263@stud.uni-stuttgart.de>
2021-01-08 11:54:33 +0100

    project IDs from the client table deleted

--- api/src/main/java/entities/Client.java
+++ api/src/main/java/entities/Client.java
@@ -2,7 +2,6 @@ package entities;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-import org.apache.commons.lang3.StringUtils;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -38,8 +37,6 @@ public class Client {
     @DatabaseField(columnName = "ID of the contact person")
     private Integer contactPersonID;
 
-    @DatabaseField(columnName = "IDs of the projects")
-    private String projectIDs;
 
     @DatabaseField(columnName = "Address")
     private String address;
@@ -67,13 +64,21 @@ public class Client {
         this.address = address;
     }
 
+
     public Integer getClientID() {
         return clientID;
     }
 
+    public void setClientID(Integer clientID) {
+        this.clientID = clientID;
+    }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -98,61 +103,6 @@ public class Client {
 
     public void setContactPersonID(Integer contactPersonID) {
         this.contactPersonID = contactPersonID;
-    }
-
-    public String getProjectIDs() {
-        return projectIDs;
-    }
-
-    public void addProject(Integer projectID) {
-        if (this.projectIDs.equals("")) {
-            this.projectIDs = projectID.toString();
-        } else {
-            this.projectIDs += "-" + projectID;
-        }
-    }
-
-    /**
-     * a project ID  will be removed from the list of projects of the clients.
-     * If this competence the first saved competence and the employee has other
-     * competences, then both the competence and the delimiter symbol after it will be removed.
-     * if the employee has many competences then both the competence and the delimiter
-     * symbol before it will be removed, else the employee has only this competence and
-     * after removing it he will have no qualities more
-     * <p>
-     * IMPORTANT : WE MUST CHECK WHETHER THE PROJECT CAN BE DELETED
-     *
-     * @param projectIDToRemove competence to be removed from the list
-     *                          of competences of the employee
-     */
-    public void deleteProject(Integer projectIDToRemove) {
-        if (!this.projectIDs.contains(projectIDToRemove.toString())) {
-            System.out.println("This client does not have this project");
-        }
-
-        if (this.projectIDs.contains(projectIDToRemove + "-")) {
-            this.projectIDs = this.projectIDs.replace(projectIDToRemove + "-", "");
-        } else if (this.projectIDs.contains("-" + projectIDToRemove)) {
-            this.projectIDs = this.projectIDs.replace("-" + projectIDToRemove, "");
-        } else {
-            this.projectIDs = "";
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "clientID=" + clientID +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", telefonNumber='" + telephoneNumber + '\'' +
-                ", contactPersonID='" + contactPersonID + '\'' +
-                ", projectIDs='" + projectIDs + '\'' +
-                '}' + "\n";
-    }
-
-    public void setID(int id) {
-        this.clientID = id;
     }
 
     public String getAddress() {
